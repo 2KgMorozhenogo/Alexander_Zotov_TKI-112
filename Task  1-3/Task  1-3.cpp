012a6c0f73adfcab48f19171385ff50c6fde57db
2KgMorozhenogo <sasha.zotov.2002@gmail.com>
2021-05-21 07:55:55 +0300

    Добавлены файлы:
    1)1-3.cpp
    2)1-3.sln
    3)1-3.vcxproj
    4)1-3.vcxproj.filters

---
+++ Task  1-3/Task  1-3.cpp
@@ -0,0 +1,38 @@
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+/**
+* \brief Расчет давления pressure 
+* \param g - Ускорение свободного падения
+* \param massa - Масса
+* \param square - Площадь поверхности опоры
+* \return Pressure - Давление
+*/
+double GetPressure(const double massa, const double square);
+
+/* 
+* /brief Точка входа в программу
+* /return Код ошибки (0-успех)
+*/
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите масу объекта - ";
+    double massa;
+    cin >> massa;
+    cout << "Введите площадь - ";
+    double square;
+    cin >> square;
+    double pressure = GetPressure(massa, square);
+    cout << "Давление = " << pressure;
+
+    return 0;
+}
+
+double GetPressure(const double massa, const double square)
+{
+    const double g = 9.80665;
+    return  (massa * g) / square;
+}
