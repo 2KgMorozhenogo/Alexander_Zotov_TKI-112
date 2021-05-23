f0b2eb154667ec1092c9a6488a48ef0f82f1b726
2KgMorozhenogo <sasha.zotov.2002@gmail.com>
2021-05-23 21:32:20 +0300

    Добавлено задание 2-2:
    Добавлены файлы:
    1)2-2.cpp
    2)2-2.sln
    3)2-2.vcxproj
    4)2-2.vcxproj.filters

---
+++ 2-2/2-2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/**
+* \brief Цель
+* \param
+* \param
+* \param
+* \return Конечное значение
+*/
+void input();
+void solution();
+
+
+const int a = 2.5;
+double x, y;
+
+void solution()
+{
+    if (x > a)
+    {
+        y = (x * (pow(sin(x), 2)));
+        std::cout << "Значение y равно - " << y << endl;
+    }
+    else
+    {
+        y = (x * (pow(cos(x), 2)));
+        std::cout << "Значение y равно - " << y << endl;
+    }
+}
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите x - ";
+    cin >> x;
+    solution();
+    return 0;
+}
