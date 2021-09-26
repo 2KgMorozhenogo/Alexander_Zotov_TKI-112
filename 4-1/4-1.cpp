c16be161052002d8f7ab1da1b0f78f2b4972cf88
Александр Зотов ТКИ-112 <81771070+2KgMorozhenogo@users.noreply.github.com>
2021-09-26 18:10:37 +0300

    Update 4-1.cpp

--- 4-1/4-1.cpp
+++ 4-1/4-1.cpp
@@ -1,176 +1,210 @@
 #include <iostream>
-#include <ctime>
+#include <string>
+#include <random>
+#include <sstream>
+
 using namespace std;
 
-/*
-* \brief - заполнение массива пользователем
-* \return - заполненный массив
-*/
-int UserInput(int* arr, int size);
+/**
+* \brief Заполнение массива случайными числами
+* \array Массив
+* \size Размер массива
+* \param min Минимально возможное число
+* \param max Максимально возможное число
+**/
+void randomizeArray(int* array, const size_t size, const int min, const int max);
 
-/*
-* \brief = заполнение массива случайными числами
-* \return - заполненный массив
-*/
-int randomDigits(int* arr, int size);
+/**
+* \brief Ввод массива с клавиатуры
+* \param array Массив
+* \param size Размер массива
+**/
+void manualInput(int* array, const size_t size);
 
-/*
-* \brief - вывод массива
-*/
-void output(int* arr, int size);
+/**
+* \brief Конвертация массива строку вида {element_1, element_2, ... ,element_n}
+* \param array Массив
+* \param size Размер массива
+* \param message Сообщение, которое будет выведено перед массивом
+**/
+string arrayToString(int* array, const size_t size, const string& message = "");
 
-/*
-* \brief - вычисление суммы отрицательных элементов кратных 10
-* \param total - сумма отрицательных элементов кратных 10
-* \param counter - счётчик
-* \return - сумма отрицательных элементов кратных 10
-*/
-int sum(int* arr, int size);
+/**
+* \brief Определяет чётное ли число
+* \param number Число, которое необходимо проверить
+* \return True если чётное
+**/
+bool isEven(const int number);
 
-/*
-* \brief - переворот первых k элементов массива
-* \param quantity - число k
-* \param buffer - буфферная переменная
-* \param counter - счётчик
-* \return - перевёрнутый массив
-*/
-int swapMyArrayPlease(int* arr, int size);
+/**
+* \brief Определяет делится ли число на делитель
+* \param number Число, которое необходимо проверить
+* \param divider Делитель
+* \return True если делится без остатка
+**/
+bool isDivisible(const int number, const int divider);
 
-/*
-* \brief - нахождение пары соседних элементов с произведением, равным заданному числу.
-* \param couple - заданное число для поиска пар с произведением
-* \param counter - счётчик
-* \return - массив
-*/
-int multiplication(int* arr, int size);
+/**
+* \brief Считает сумму отрицательных элементов, значения которых кратно 10
+* \param arr Массив
+* \param size Размер массива
+* \return Сумма элементов
+**/
+int calcSum(int* arr, const size_t size);
 
-enum class path
+/**
+* \brief Замена первых k элементов массива на те же элементы в обратном порядке.
+* \param arr Массив
+* \param size Размер массива
+* \param index первые k элементы (нумерация начинается с нуля)
+**/
+void replaceElements(int* arr, const size_t size, const size_t index);
+
+/**
+* \brief Определяет, есть ли пара соседних элементов с произведением, равным заданному числу.
+* \param arr Массив
+* \param size Размер массива
+* \param number Заданное число
+* \return True если существует
+**/
+bool pairExist(int* arr, const size_t size, const int number);
+
+
+/**
+* \brief Выбор заполнеия массива
+**/
+enum class FillType
 {
-	Manuall = 1,
-	Random = 2
+    randomize = 1,
+    manual = 2
 };
 
-/*
-* \brief Точка входа в программу
-* \return Код ошибки(0 - успех)
-*/
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	cout << "Введите количество элементов массива - ";
-	int size;
-	cin >> size;
-	int* arr;
-	arr = new int[size];
+    const int MIN = -1000, MAX = 1000;
 
-	cout << "Как хотите заполнить массив?" << '\n' << "Введите 1 для заполнения массива с клавиатуры или 2 для заполнения массива случайными числами - ";
-	int choice;
-	cin >> choice;
-	const auto filling = static_cast<path>(choice);
-	switch (filling) {
-	case path::Random:
-	{
-		randomDigits(arr, size);
-		break;
-	}
-	case path::Manuall:
-	{
-		UserInput(arr, size);
-		break;
-	}
-	default:
-	{
-		cout << "Некоректный ввод.";
-		return 0;
-	}
-	}
+    cout << "Please input array length: ";
+    size_t size;
+    cin >> size;
 
-	sum(arr, size);
-	swapMyArrayPlease(arr, size);
-	multiplication(arr, size);
+    int* arr = new int[size];
 
-	return 0;
+    cout << "Choose an option:\n" << "1) Generate array\n" << "2) Manual input\n" << "1 or 2: ";
+    int choice;
+    cin >> choice;
+
+    const auto fillType = static_cast<FillType>(choice);
+
+    switch (fillType)
+    {
+    case FillType::randomize:
+    {
+        randomizeArray(arr, size, MIN, MAX);
+        cout << arrayToString(arr, size, "Your array = ") << "\n";
+        break;
+    }
+    case FillType::manual:
+    {
+        manualInput(arr, size);
+        break;
+    }
+    default:
+        break;
+    }
+
+    cout << "Сумма отрицательных элементов, значения которых кратно 10" << calcSum(arr, size) << "\n";
+
+    cout << "Задайте число: ";
+    int num;
+    cin >> num;
+    if (pairExist(arr, size, num))
+        cout << "Пара существует\n";
+    else
+        cout << "Пары не существует\n";
+
+    cout << "Сколько первых элементов заменить? ";
+    size_t k;
+    cin >> k;
+    replaceElements(arr, size, --k);
+    cout << "Замена первых " << k << " элементов массива на те же элементы в обратном порядке: " << arrayToString(arr, size) << "\n";
+
+    delete[] arr;
+    
 }
 
-int randomDigits(int* arr, int size)
+void randomizeArray(int* array, const size_t size, const int min, const int max)
 {
-	const int UP_BOUND = 1000;
-	const int LOW_BOUND = -1000;
-	srand(time(NULL));
-	int counter = 0;
-	while (counter++ < size)
-		arr[counter] = rand() % (2 * UP_BOUND + 1) + LOW_BOUND;
-	output(arr, size);
-	return(arr[size]);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    const uniform_int_distribution<> uniformIntDistribution(min, max);
+
+    for (size_t i = 0; i < size; ++i)
+        array[i] = uniformIntDistribution(gen);
+
 }
 
-int UserInput(int* arr, int size)
+void manualInput(int* array, const size_t size)
 {
-	int counter = 0;
-	while (counter++ < size)
-		cin >> arr[counter];
-	output(arr, size);
-	return(arr[size]);
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << "array[" << i << "] = ";
+        cin >> array[i];
+    }
 }
 
-void output(int* arr, int size)
+string arrayToString(int* array, const size_t size, const string& message)
 {
-	cout << "Ваш массив: ";
-	int counter = 0;
-	while (counter++ < size)
-		cout << arr[counter] << " ";
+    std::stringstream buffer;
+    buffer << message << "{" << array[0];
+
+    for (size_t i = 1; i < size; ++i)
+    {
+        buffer << ", " << array[i];
+    }
+
+    buffer << "}";
+
+    return buffer.str();
 }
 
-int sum(int* arr, int size)
+bool isEven(const int number)
 {
-	int total = 0;
-	int counter = 0;
-	while (counter++ < size)
-	{
-		if ((arr[counter] < 0) && (arr[counter] % 10 == 0))
-			total += arr[counter];
-	}
-	if (total == 0)
-		cout << '\n' << "В массиве нет отрицательных чисел кратных 10.\n";
-	else
-		cout << '\n' << "Сумма отрицательных элементов массива кратных 10: " << total << ".\n";
-	return total;
+    return number % 2 == 0;
 }
 
-int swapMyArrayPlease(int* arr, int size)
+bool isDivisible(const int number, const int divider)
 {
-	int quantity;
-	int buffer;
-	int counter = 1;
-	cout << "Введите количество первых элементов, которое заменятся на те же переменные, но в обратном порядке: ";
-	cin >> quantity;
-	for (counter; counter < quantity; counter++, quantity--)
-	{
-		buffer = arr[counter];
-		arr[counter] = arr[quantity];
-		arr[quantity] = buffer;
-	}
-	output(arr, size);
-	return arr[counter];
+    return number % divider == 0;
 }
 
-int multiplication(int* arr, int size)
+int calcSum(int* arr, const size_t size)
 {
-	cout << "\nВведите число, которое будет значением произведения пар: ";
-	int couple;
-	cin >> couple;
-	int counter = 0;
-	int out = 1;
-	cout << "Найдены пары: ";
-	for (counter; counter < size; counter++)
-	{
-		if (arr[counter] * arr[counter + 1] == couple)
-		{
-			cout << "[" << arr[counter] << " " << arr[counter + 1] << "]" << " ";
-			out++;
-		}
-	}
-	if (out == 1)
-		cout << "пар, произведение которых равно " << couple << ", нет.";
-	return arr[size];
+    int sum = 0;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        if (arr[i] < 0 && isDivisible(arr[i], 10))
+            sum += arr[i];
+    }
+
+    return sum;
+}
+
+void replaceElements(int* arr, const size_t size, const size_t index)
+{
+    if (index >= size)
+        throw out_of_range("Индекс не может быть больше размера массива");
+
+    for (size_t i = 0; i <= index / 2; i++)
+        swap(arr[i], arr[index - i]);
+        
+}
+
+bool pairExist(int* arr, const size_t size, const int number)
+{
+    for (size_t i = 0; i < size - 1; i++)
+        if (arr[i] * arr[i + 1] == number)
+            return true;
+
+    return false;
 }
