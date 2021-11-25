b970e8888ec1dca19af213dac2fd55bac802ca0b
Александр Зотов ТКИ-112 <81771070+2KgMorozhenogo@users.noreply.github.com>
2021-11-25 23:26:02 +0300

    Update 3-1.cpp

--- 3-1/3-1.cpp
+++ 3-1/3-1.cpp
@@ -10,19 +10,29 @@ using namespace std;
 * \return Значение функции
 **/
 double calcFunction(const double x);
+/**
+* \brief Проверка существования функции
+* \param x Аргумент функции
+* \return Проверка значения
+**/
+bool isCalculated(const double x);
 
 int main()
-{
-	const double LOWER_BOUND = 2.0, UPPER_BOUND = 4.0, STEP = 0.2;
+{	
+	setlocale(LC_ALL, "Russian");
+	cout << fixed;
+	const double LOWER_BOUND = -1.0, UPPER_BOUND = 4.0, STEP = 0.2;
 	const size_t WIDTH = 5;
 
 	cout << setw(WIDTH) << "X" << " | " << setw(2 * WIDTH) << "Y\n";
 
 	for (double x = LOWER_BOUND; x < UPPER_BOUND + STEP; x += STEP)
 	{
-		cout << setw(WIDTH) << x << " | " << setw(2 * WIDTH) << calcFunction(x) << "\n";
+		if (isCalculated(x))
+			cout << setw(WIDTH) << x << " | " << setw(2 * WIDTH) << "Невозможно вычислить значение в данной точке\n";
+		else
+			cout << setw(WIDTH) << x << " | " << setw(2 * WIDTH) << calcFunction(x) << "\n";
 	}
-
 	return 0;
 }
 
@@ -31,3 +41,7 @@ double calcFunction(const double x)
 	return 3 * x - 4 * log(x) - 5;
 }
 
+bool isCalculated(const double x)
+{
+	return (x <= 0);
+}
